eab159048ebf71ed3ca8d17d20d78881f001971b
Manav Doshi <200100094@iitb.ac.in>
2022-01-15 12:43:23 +0530

    Adding backend files

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Team
 # Register your models here.
+# class PlayerAdmin(admin.ModelAdmin):
+# 	list_display = ('user', 'current_level', 'current_level_time','points', 'bonus_attempted')
+# class userdataadmin(admin.ModelAdmin):
+# 	list_display=('username','email','roll','referral_count')
+# admin.site.register( userdata, userdataadmin)
+
+class TeamAdmin(admin.ModelAdmin):
+	list_display = ('leader_name', 'leader_roll_number')
+
+admin.site.register(Team, TeamAdmin)
