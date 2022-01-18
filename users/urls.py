42601eb0ffae68947e526b78890c80b8463d50cf
muskanbhutra <bhutramuskan29@gmail.com>
2022-01-18 18:51:52 +0530

    adding login and game

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .import views
+from .views import Play
 
 urlpatterns=[
 	path('',views.home, name='home'),
 	path('faq',views.faq, name='faq'),
-	
+	path('play',Play.as_view(), name='play'),
+	path('generatepassword',views.generatepassword, name='generatepassword'),
+	path('login', views.login1, name='login'),
+
 ]
