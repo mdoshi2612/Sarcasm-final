3ca5bbafc464cbc81ca459a59d25d1a8089c9531
Manav Doshi <200100094@iitb.ac.in>
2022-01-18 13:47:56 +0530

    Adding table

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Team(models.Model):
 	leader_name = models.CharField(max_length=100)
-	leader_roll_number = models.CharField(max_length=100)
+	leader_roll_number = models.CharField(max_length=100, primary_key=True)
 	year_of_study = models.CharField(max_length=100)
 	team_name = models.CharField(max_length=100)
 	team_logo = models.CharField(max_length=100, null=True)
@@ -11,3 +11,4 @@ class Team(models.Model):
 	player2 = models.CharField(max_length=100)
 	player3 = models.CharField(max_length=100)
 	player4 = models.CharField(max_length=100)
+	score = models.IntegerField(default=0)
