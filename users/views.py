0c036b84829f992f6045e3bc6c1d6e7ad434face
Manav Doshi <200100094@iitb.ac.in>
2022-01-21 09:40:27 +0530

    Changes in file locations

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Team, Level, BonusQuestion
@@ -15,7 +17,9 @@ from django.views import View
 from django.utils import timezone
 from django.core.mail import send_mail, BadHeaderError, EmailMessage
 import csv
-from .image_overlay import generate_image
+from django.templatetags.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from PIL import Image, ImageDraw, ImageFont
 
 # Create your views here.
 
@@ -334,6 +338,73 @@ def increase_bonus_level(request) :
 		context = {'top_teams': top_teams}
 		return render(request,'users/leaderboard.html', context)
 	return render(request,'users/leaderboard.html')
+
+def generate_image(pokemon, team_name):
+    # Front Image
+    filename1 = os.path.join(settings.STATIC_ROOT, '/images/final1-01.png')
+    
+    filename = staticfiles_storage.url('pokemons/'+pokemon+'.png')
+    
+    # Open Background Image
+    background = Image.open(filename1)
+    
+    # Open Front Image
+    frontImage = Image.open(filename)
+    
+
+
+    # Convert image to RGBA
+    frontImage = frontImage.convert("RGBA")
+    #frontImage.show()
+
+
+    # Convert image to RGBA
+    background = background.convert("RGBA")
+    #background.show()
+
+    
+    # Calculate width to be at the center
+    width = (background.width - frontImage.width) // 2
+    # Calculate height to be at the center
+    height = (background.height - frontImage.height) // 2
+    width_original,height_original=frontImage.size
+    factor_1=1.3
+    factor_2=1.3
+
+    newsize = (int(factor_1*width_original),int(factor_2*width_original))
+    frontImageScaled = frontImage.resize(newsize)
+
+    # Paste the frontImage at (width, height)
+    background.paste(frontImageScaled, (1900, 2300), frontImageScaled)
+    #background.show()
+
+
+
+    txt = Image.new("RGBA", background.size, (255, 255, 255, 0))
+
+    # get a font
+    fnt = ImageFont.truetype("AEH.ttf", 250)
+    # get a drawing context
+
+
+    x = 0
+
+    if(len(team_name) <= 10):
+        x = 2000
+    elif(len(team_name) <= 17):
+        x = 1500
+    elif(len(team_name) <= 26):
+        x = 1300 
+    else:
+        x = 1000 
+
+    d = ImageDraw.Draw(txt)
+
+    d.text((x, 1900), team_name, font=fnt, fill=(255, 191, 0, 255))
+
+    out = Image.alpha_composite(background, txt)
+    return out
+
 
 def image(request):
 	response = HttpResponse(content_type='image/png')
